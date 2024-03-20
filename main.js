1987c0e9a9d47fb0949efff1ec812537f4720776
Samir <122364498+Stileexx@users.noreply.github.com>
2024-03-20 23:26:19 +0100

    js for hamburger menu

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 let header = document.querySelector("header");
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+  navbar.classList.toggle("active");
+};
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
 
 window.addEventListener("scroll", () => {
   header.classList.toggle("active", window.scrollY > 0);
